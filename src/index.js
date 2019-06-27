16c2573a6a62de71f0f24e82f61947c7c576abcb
Bogus Franczak <bfranczak@gmail.com>
2019-06-27 20:17:56 +0200

    Create footer layout

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import 'bootstrap/dist/css/bootstrap-grid.css'
+import 'bootstrap/dist/css/bootstrap-grid.css'
 import './index.css';
 import App from './components/App';
 
-const testMessage = 'Hello, World!';
+const rootElement = document.getElementById('root');
 
-ReactDOM.render(<App/>,
-    document.getElementById('root'));
+ReactDOM.render(<App />, rootElement);
