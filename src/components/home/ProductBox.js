ca1776eb180f7d4d072ffd87b9e2886375e21dc0
Bogus Franczak <bfranczak@gmail.com>
2019-07-02 20:36:19 +0200

    Correct product box styles and layout

--- src/components/home/ProductBox.js
+++ src/components/home/ProductBox.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons';
 import { Link } from 'react-router-dom'
 import './ProductBox.css';
 
 const ProductBox = props => {
-  const imgUrl = 'https://via.placeholder.com/200';
+  const imgUrl = 'https://via.placeholder.com/400x200';
   return (
     <div className="col-md-6 col-sm-12">
       <div className="product-box">
@@ -13,7 +14,7 @@ const ProductBox = props => {
           <span className="optional-message">Optional message</span>
           <span className="favourite-marker visible-on-hover">
             <a href="#">
-              <FontAwesomeIcon size="1x" icon={faHeart} />
+              <FontAwesomeIcon className="favourite-marker-ico" icon={faHeartRegular} />
             </a>
           </span>
         </div>
@@ -23,8 +24,8 @@ const ProductBox = props => {
           </Link>
         </div>
         <div className="product-box-footer">
-          <h2 className="product-name change-on-hover">{props.product.name}</h2>
-          <div>
+          <h3 className="product-name change-on-hover">{props.product.name}</h3>
+          <div className="footer-bottom">
             <span className="base-price change-on-hover">{props.product.basePrice} $</span>
             <a href="#" className="add-to-cart visible-on-hover">
               <FontAwesomeIcon icon={faShoppingBag}></FontAwesomeIcon>
