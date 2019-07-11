d28dc9560e5bf1bdbe71105185a0c05823b43885
Bogus Franczak <bfranczak@gmail.com>
2019-07-11 19:21:39 +0200

    Register store and redux thunk

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap-reboot.css'
-import 'bootstrap/dist/css/bootstrap-grid.css'
-// import './assets/MyriadPro/style.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers/index';
+import 'bootstrap/dist/css/bootstrap-reboot.css';
+import 'bootstrap/dist/css/bootstrap-grid.css';
 import './index.css';
 import App from './components/App';
+import { fetchProducts } from './actions/products';
+// TEST IMPORT ONLY REMOVE
 
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk)
+);
+
+store.dispatch(fetchProducts());
 const rootElement = document.getElementById('root');
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement);
